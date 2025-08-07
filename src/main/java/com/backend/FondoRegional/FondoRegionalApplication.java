5951a1bfe18bbf90cea2557e12344c1deefb06a0
Camilo Mora <camiloalf23@gmail.com>
2025-08-07 11:21:57 -0500

    Estructura inicial

---
+++ src/main/java/com/backend/FondoRegional/FondoRegionalApplication.java
@@ -0,0 +1,13 @@
+package com.backend.FondoRegional;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FondoRegionalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FondoRegionalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend/FondoRegional/FondoRegionalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.FondoRegional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FondoRegionalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
